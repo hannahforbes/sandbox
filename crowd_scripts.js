e2e21f9b0d666a900df6f95a610ed7bef03ba344
Hannah Forbes <hannahforbesmail@gmail.com>
2017-08-25 16:52:32 +0100

    bringing duplicate up to date

---
+++ crowd_scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+    setInterval(function()
+      {
+        function dataKickstarter(dataUrl, projectName, dataZone1, dataZone2){
+        $.ajax({
+          url: dataUrl,
+          context: document.body
+        }).done(function(result) {
+          var backers = result.project.backers_count;
+          var pledged = result.project.pledged;
+          var time = result.project.state_changed_at;
+          console.log(", " + projectName + ", " + "Backers = " + backers + ", ", "Pledged = " + pledged + ",");
+          document.getElementById(dataZone1).innerHTML = "Backers = " + backers;
+          document.getElementById(dataZone2).innerHTML = "Pledged = " + pledged;
+        });
+      }
+      dataKickstarter($('#data-zone1').attr('data-url'), 'History Poster', 'data-zone1', 'data-zone2');
+      dataKickstarter($('#data-zone3').attr('data-url'), 'Sun Spots', 'data-zone3', 'data-zone4');
+      dataKickstarter($('#data-zone5').attr('data-url'), 'Orrery Lamp', 'data-zone5', 'data-zone6');
+      dataKickstarter($('#data-zone7').attr('data-url'), 'Apricoat', 'data-zone7', 'data-zone8');
+      dataKickstarter($('#data-zone9').attr('data-url'), 'Nise Wave', 'data-zone9', 'data-zone10');
+      dataKickstarter($('#data-zone11').attr('data-url'), 'Wooden World Watch', 'data-zone11', 'data-zone12');
+    }, 10000) //10000 = 10s
+
+});
