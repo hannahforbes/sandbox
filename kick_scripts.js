f4239bea9a83a2aad1fa23394ae3ffb75599d4f2
Hannah Forbes <hannahforbesmail@gmail.com>
2017-08-25 13:33:52 +0100

    Generalising Ajax request, currently successful

--- kick_scripts.js
+++ kick_scripts.js
@@ -14,16 +14,19 @@ $(document).ready(function() {
       setInterval(function()
       {
         //World History Poster
+        var dataUrl = $('#data-zone1').attr('data-url');
         $.ajax({
-          url: "http://www.kickstarter.com/projects/usefulcharts/timeline-of-world-history-poster-projects-of-earth/stats.json?v=1",
+          url: dataUrl,
           context: document.body
         }).done(function(result) {
+          var dataZone1 = 'data-zone1';
+          var dataZone2 = 'data-zone2';
           var backers = result.project.backers_count;
           var pledged = result.project.pledged;
           var time = result.project.state_changed_at;
           console.log(", History Poster, ", "Backers = " + backers + ", ", "Pledged = " + pledged + ",");
-          document.getElementById('data-zone1').innerHTML = "Backers = " + backers;
-          document.getElementById('data-zone2').innerHTML = "Pledged = " + pledged;
+          document.getElementById(dataZone1).innerHTML = "Backers = " + backers;
+          document.getElementById(dataZone2).innerHTML = "Pledged = " + pledged;
         });
 
         //Sun Spots
