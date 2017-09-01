0ce5111f758e7780f27903206e248475e552eb80
Hannah Forbes <hannahforbesmail@gmail.com>
2017-09-01 16:18:05 +0100

    Div inserts on HTML updated to be more reusable

--- crowd_scripts.js
+++ crowd_scripts.js
@@ -17,30 +17,28 @@ $(document).ready(function() {
       var projTitleFinal = projPuncRemove.replace(/\s+/g, '-').toLowerCase();
       return projTitleFinal;
     }
-
-
   }
 
   //Takes Project Creator (copied from KS) and allows it to be inserted into url
   function projCreatorToUrl(projCreator) {
-
-    if (projCreator.indexOf(" ") > -1){
+    //Allows a Project Creator's name to include a maximum of 5 words, while loop crashed browser
+    if (projCreator.indexOf(" ") > -1) {
       var projCreatorSpaceRemove = projCreator.replace(" ", "").toLowerCase();
     } else {
       return projCreator;
     }
-    if (projCreatorSpaceRemove.indexOf(" ") > -1){
+    if (projCreatorSpaceRemove.indexOf(" ") > -1) {
       var projCreatorSpaceRemoveAgain = projCreatorSpaceRemove.replace(" ", "").toLowerCase();
     } else {
       return projCreatorSpaceRemove;
     }
-    if (projCreatorSpaceRemoveAgain.indexOf(" ") > -1){
+    if (projCreatorSpaceRemoveAgain.indexOf(" ") > -1) {
       var projCreatorSpaceRemoveAgainAgain = projCreatorSpaceRemoveAgain.replace(" ", "").toLowerCase();
     } else {
       return projCreatorSpaceRemoveAgain;
     }
 
-    if (projCreatorSpaceRemoveAgainAgain.indexOf(" ") > -1){
+    if (projCreatorSpaceRemoveAgainAgain.indexOf(" ") > -1) {
       var projCreatorSpaceRemoveAgainAgainAgain = projCreatorSpaceRemoveAgainAgain.replace(" ", "").toLowerCase();
     } else {
       return projCreatorSpaceRemoveAgainAgain;
@@ -53,8 +51,7 @@ $(document).ready(function() {
     return projNameSplit[0];
   }
 
-  //function dataKickstarter(projectCreator, projectTitle, projectName, dataZone1, dataZone2, dataZone3) {
-  function dataKickstarter(projectCreator, projectTitle, projectName, dataZone1, dataZone2) {
+  function dataKickstarter(projectCreator, projectTitle, projectName, dataZone1, dataZone2, dataZone3) {
     $.ajax({
       url: 'https://www.kickstarter.com/projects/' + projectCreator + '/' + projectTitle + '/stats.json',
       context: document.body
@@ -62,20 +59,25 @@ $(document).ready(function() {
       var backers = result.project.backers_count;
       var pledged = result.project.pledged;
       var time = result.project.state_changed_at;
-      console.log(", " + projectName + ", " + backers + ", ", + pledged);
-      document.getElementById(dataZone1).innerHTML = "Backers = " + backers;
-      document.getElementById(dataZone2).innerHTML = "Pledged = " + pledged;
-      //document.getElementById(dataZone3).innerHTML = projectName;
+      console.log(", " + projectName + ", " + backers + ", ", +pledged);
+      //Inserting on HTML Page
+      document.getElementById(dataZone1).innerHTML = projectName;
+      document.getElementById(dataZone2).innerHTML = "Backers = " + backers;
+      document.getElementById(dataZone3).innerHTML = "Pledged = " + pledged;
     });
   }
 
-  for (i = 0; (i < projectTitles.length); i++) {
-    dataKickstarter(projCreatorToUrl(projectCreators[i]), projTitleToUrl(projectTitles[i]), projSimpleName(projectTitles[i]), 'data-zone' + i, 'data-zone' + i + 'A');
+  for (i = 0;
+    (i < projectTitles.length); i++) {
+    dataKickstarter(projCreatorToUrl(projectCreators[i]), projTitleToUrl(projectTitles[i]), projSimpleName(projectTitles[i]),
+      'data-zone' + i, 'data-zone' + i + 'A', 'data-zone' + i + 'B'); //Inserting on HTML page
   }
 
   setInterval(function() {
-    for (i = 0; (i < projectTitles.length); i++) {
-      dataKickstarter(projCreatorToUrl(projectCreators[i]), projTitleToUrl(projectTitles[i]), projSimpleName(projectTitles[i]), 'data-zone' + i, 'data-zone' + i + 'A');
+    for (i = 0;
+      (i < projectTitles.length); i++) {
+      dataKickstarter(projCreatorToUrl(projectCreators[i]), projTitleToUrl(projectTitles[i]), projSimpleName(projectTitles[i]),
+        'data-zone' + i, 'data-zone' + i + 'A', 'data-zone' + i + 'B'); //Inserting on HTML page
     }
   }, 10000) //10000 = 10s, 60000 = 1m, 3600000 = 1h, 86400000 = 1d
 
