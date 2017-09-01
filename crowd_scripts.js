de9f58e3fddfd4c56c9ed47c8ed6d8e3f10e0a4b
Hannah Forbes <hannahforbesmail@gmail.com>
2017-09-01 12:17:40 +0100

    Condensing URL for reusability

--- crowd_scripts.js
+++ crowd_scripts.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
 
     setInterval(function()
       {
-        function dataKickstarter(dataUrl, projectName, dataZone1, dataZone2){
+        function dataKickstarter(projectCreator, projectTitle, projectName, dataZone1, dataZone2){
         $.ajax({
-          url: dataUrl,
+          url: 'https://www.kickstarter.com/projects/' + projectCreator + '/' + projectTitle + '/stats.json',
           context: document.body
         }).done(function(result) {
           var backers = result.project.backers_count;
@@ -15,12 +15,12 @@ $(document).ready(function() {
           document.getElementById(dataZone2).innerHTML = "Pledged = " + pledged;
         });
       }
-      dataKickstarter($('#data-zone1').attr('data-url'), 'History Poster', 'data-zone1', 'data-zone2');
-      dataKickstarter($('#data-zone3').attr('data-url'), 'Sun Spots', 'data-zone3', 'data-zone4');
-      dataKickstarter($('#data-zone5').attr('data-url'), 'Orrery Lamp', 'data-zone5', 'data-zone6');
-      dataKickstarter($('#data-zone7').attr('data-url'), 'Apricoat', 'data-zone7', 'data-zone8');
-      dataKickstarter($('#data-zone9').attr('data-url'), 'Nise Wave', 'data-zone9', 'data-zone10');
-      dataKickstarter($('#data-zone11').attr('data-url'), 'Wooden World Watch', 'data-zone11', 'data-zone12');
+      dataKickstarter('mattbaker','timeline-of-world-history-poster-projects-of-earth', 'History Poster', 'data-zone1', 'data-zone2');
+      // dataKickstarter($('#data-zone3').attr('data-url'), 'Sun Spots', 'data-zone3', 'data-zone4');
+      // dataKickstarter($('#data-zone5').attr('data-url'), 'Orrery Lamp', 'data-zone5', 'data-zone6');
+      // dataKickstarter($('#data-zone7').attr('data-url'), 'Apricoat', 'data-zone7', 'data-zone8');
+      // dataKickstarter($('#data-zone9').attr('data-url'), 'Nise Wave', 'data-zone9', 'data-zone10');
+      // dataKickstarter($('#data-zone11').attr('data-url'), 'Wooden World Watch', 'data-zone11', 'data-zone12');
     }, 10000) //10000 = 10s
 
 });
