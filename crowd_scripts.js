4736ea30f9392dc102795c7bdffc0430e920579f
Hannah Forbes <hannahforbesmail@gmail.com>
2017-09-01 14:45:39 +0100

    function 'projTitleToUrl' created to allow copy and paste from Kickstarter to be inputted into data url

--- crowd_scripts.js
+++ crowd_scripts.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
 
+  function projTitleToUrl (projTitle){
+  //  var projTitle = "Timeline of World History Poster - Projects of Earth";
+    var projPuncRemove = projTitle.replace("'", "");
+    var projTitleSplit = projPuncRemove.split(/\s*\-\s*/g)
+    var projTitleSplitFirst = projTitleSplit[0].replace(/\s+/g, '-').toLowerCase();
+    var projTitleSplitSecond = projTitleSplit[1].replace(/\s+/g, '-').toLowerCase();
+    var projTitleFinal = projTitleSplitFirst + '-' + projTitleSplitSecond;
+    return projTitleFinal;
+  }
+
   function dataKickstarter(projectCreator, projectTitle, projectName, dataZone1, dataZone2, dataZone3) {
     $.ajax({
       url: 'https://www.kickstarter.com/projects/' + projectCreator + '/' + projectTitle + '/stats.json',
@@ -25,5 +35,9 @@ $(document).ready(function() {
     dataKickstarter('robjmadin', 'steamroller-album', 'Steam Roller', 'data-zone5', 'data-zone6', 'data-zoneC');
     dataKickstarter('opengoldberg', 'libre-art-of-the-fugue', 'Libre Art of the Fugure', 'data-zone7', 'data-zone8', 'data-zoneD');
   }, 10000) //10000 = 10s, 60000 = 1m, 3600000 = 1h, 86400000 = 1d
+
+
+
+console.log(projTitleToUrl("Turtle Rover - World's First Earth Rover"));
 
 });
