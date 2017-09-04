52ce8f12d8d8b2b5c10d5002c1be82487b99e77c
Hannah Forbes <hannahforbesmail@gmail..com>
2017-09-04 16:44:01 +0100

    App.js refreshed to learn buttons so second duplicate created

--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
 import React from 'react';
 
 class App extends React.Component{
-  constructor(){
-    super();
-    this.state = {
-      txt: 'this is the state txt'
-    }
-  }
-  update( e ){
-    this.setState({txt: e.target.value})
-  }
-
-
   render(){
-
-    let txt = this.props.txt
-    return (
-      <div>
-        <h1>{this.state.txt}</h1>
-        <Widget update={this.update.bind(this)}/>
-      </div>
-    )
+    return <Button>React</Button>
   }
 }
 
-const Widget = (props) =>
-<input type="text" onChange={props.update}/>
-//On 1st duplicate this can be found in a detailed simplified way
-//You can see "update" added as an "attribute" (prop) to the Widget component
-//NOW In the browser, our child component is updating the state of our parent component
 export default App
