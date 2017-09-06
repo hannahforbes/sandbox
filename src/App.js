b9da507c33688fc44e52b0a28cb32f2d940c7960
Hannah Forbes <hannahforbesmail@gmail..com>
2017-09-06 10:56:48 +0100

    New tutorial started and changes made to App.js, first commit of the day

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-//Example of Custom propType Validation
+//React synthetic event systems
+//Which normizes all events I may want to use across various browsers
 
 import React from 'react';
 
 class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {currentEvent: '---'}
+    this.update = this.update.bind(this)
+  }
+  update(e){
+    this.setState({currentEvent: e.type})
+  }
   render(){
-    return <Title text="123456"/>
+    return (
+      <div>
+        <textarea
+          cols="30"
+          rows="10" />
+          <h1>{this.state.currentEvent}</h1>
+      </div>
+    )
   }
 }
 
-//stateless component
-const Title = (props) => <h1>Title: {props.text}</h1>
-
-Title.propTypes = {
-  text(props, propName, component){
-    if(!(propName in props)){
-      return new Error('missing ${propName}')
-    }
-    if(props[propName].length < 6){
-      return new Error('${propName} was too short')
-    }
-  }
-}
 
 export default App
 
