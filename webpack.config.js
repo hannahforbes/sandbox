de456a81fc888353c8f9669b9dfdd384f4ef4e2a
Hannah Forbes <hannahforbesmail@gmail..com>
2017-09-11 17:07:06 +0100

    Set up and started running Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,37 @@
-// This file describes everything we want Webpack to do to our files
+// This file describes everything we want Webpack to do to our files to ready them for production
+var webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: "dist/assets",
+    filename: "bundle.js",
+    publicPath: "assets"
+  },
+  devServer: {
+    inline: true,
+    contentBase: './dist',
+    port: 3000
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: ["babel-loader"],
+        query: {
+          presets: [
+            "latest",
+            "stage-0",
+            "react"
+          ]
+        }
+      }
+    ]
+  }
+}
+
+
+//'loaders' in 'module' are all the different tasks we want webpack to perform
+//'test: /\.js$/' means include all our js file, 'exclude: [..]' means exclude listed (node_modules)
+//'devServer' craetes a server that will update everytime we change things
